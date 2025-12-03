9bad763d6ef31963a3c867e65e415f730f2d00b1
Fekete Jenő <feketej@kkszki.hu>
2025-12-03 20:48:43 +0100

    Első feladat kész

---
+++ feladat1.py
@@ -0,0 +1,6 @@
+#Kérj be két számot, és írd ki az összegüket.
+
+szam1 = int(input("Kérem az első számot: "))
+szam2 = int(input("Kérem a második számot: "))
+
+print(f"A két szám összege: {szam1+szam2}")
