ce900633dbb9b4991ec6dfed48963d36ce84cbac
Fekete Jenő <feketej@kkszki.hu>
2025-12-03 22:17:39 +0100

    Valamiért traceback hibát ír

---
+++ feladat8.py
@@ -0,0 +1,16 @@
+#Kérj be egy számot és döntsd el, hogy benne van-e az előre adott listában.
+lista = [12,10,5,4,23,88,45,43,33,21]
+
+szam = int(input("Kérem a számot: "))
+
+vane = False
+
+for i in lista:
+    if lista[i] == szam:
+        vane = True
+        break
+
+if vane == True:
+    print("Van találat.")
+else:
+    print("Nincs találat.")
