c05ecf14bfeba162ed289f8d3b361b0f1aa55934
Fekete Jenő <feketej@kkszki.hu>
2025-12-03 22:28:42 +0100

    javítva az eredeti elképzelés szerint, és működik

--- feladat8.py
+++ feladat8.py
@@ -1,11 +1,13 @@
 #Kérj be egy számot és döntsd el, hogy benne van-e az előre adott listában.
+szam = 0
+
 lista = [12,10,5,4,23,88,45,43,33,21]
 
 szam = int(input("Kérem a számot: "))
 
 vane = False
 
-for i in lista:
+for i in range(len(lista)):
     if lista[i] == szam:
         vane = True
         break
